215f3d292b2569c591c260d6f79f0dfedef89ada
b <opticalfalicy@gmail.com>
2020-12-28 19:29:21 -0600

    Requesting project data from backend.
    Will work properly when Django REST is implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function loadProjects(callback){
+  const xhr = new XMLHttpRequest()
+  const method = 'GET' // 'POST'
+  const url = "http://localhost:8000/projects/"
+  const responseType = "json"
+  xhr.responseType = responseType
+xhr.open(method, url)
+xhr.onload = function() {
+  callback(xhr.response, xhr.status)
+}
+// xhr.onerror = function (e) {
+//   callback({"message": "invalid request"}, 400)
+// }
+xhr.send()
+}
+
 function App() {
+  const [projects, setProjects] = useState([{name: 123}])
+  useEffect(() => {
+    const myCallback = (response, status) => {
+
+      setProjects(response);
+
+    }
+    loadProjects(myCallback)
+  }, [])
+  
+
+  // console.log(projects.response)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {projects.map((project, index)=>{
+          return <li>{project.name}</li>
+          })} 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
